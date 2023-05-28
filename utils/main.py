bc5d5026ca0c510da44048a1236a170da3fbcab3
miwnaya <mikgerasimov@gmail.com>
2023-05-28 10:57:57 +0300

    Minor changes

--- utils/main.py
+++ utils/main.py
@@ -7,16 +7,10 @@ import pathlib
 def main():
     # определение пути к файлу
     path = pathlib.Path(pathlib.Path.cwd().parents[0], 'source files', 'operations.json')
-
-    # загрузка транзакций из файла
-    transactions = load_data(path)
-
     # формирование списка выполненных транзакций
-    validated_transactions = keep_executed(transactions)
-
+    validated_transactions = keep_executed(load_data(path))
     # формирование списка 5 последних транзакций
     needed_transactions = get_latest_transactions(validated_transactions, get_latest_transaction_numbers(validated_transactions))
-
     # вывод последних 5 транзакций
     output_transactions(needed_transactions)
 
